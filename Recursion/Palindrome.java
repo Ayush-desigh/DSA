0ebbe81d1569f7a30f782d11698a04f039fb9bf1
Ayush Kushwaha <ayushkush00@gmail.com>
2026-02-03 22:47:02 +0530

    Change in Palindrome

--- Recursion/Palindrome.java
+++ Recursion/Palindrome.java
@@ -1,24 +1,25 @@
-public class Palindrome
-{
+public class Palindrome {
 
-    static int sum=0;
-   public static void reverse(int n)
-    {
-        if(n<1)
-        {
-            return ;
+    static int sum = 0;
+
+    public static int reverse(int n) {
+        if (n == 0) {
+            return sum;
         }
-        int k=n%10;
-        sum=sum*10+k;
-        reverse(n/10);
+        int k = n % 10;
+        sum = sum * 10 + k;
+        return reverse(n / 10);
     }
-    
-//    static boolean palindrome(int n)
-//    {
-//       return n==reverse(n);
-//    }
+
+    static boolean palindrome(int n) {
+        sum = 0; // reset before each call
+        return n == reverse(n);
+    }
+
     public static void main(String[] args) {
-//        System.out.println(reverse(12345));
-//        System.out.println(palindrome(1331));
+        System.out.println(reverse(12345));   // Output: 54321
+        sum = 0; // reset before next call
+        System.out.println(palindrome(1331)); // Output: true
+        System.out.println(palindrome(1234)); // Output: false
     }
 }
