e063020a59df2f11d81007ae4a34c02423881567
Ayush Kushwaha <ayushkush00@gmail.com>
2026-02-04 00:54:35 +0530

    Can Find Index

--- Recursion/LinearSearchInArray.java
+++ Recursion/LinearSearchInArray.java
@@ -1,5 +1,11 @@
 public class LinearSearchInArray
 {
+    public static void main(String[] args) {
+        int arr[]={1,5,78,100,25,63};
+        int target=85;
+        System.out.println(searchWithIndex(arr,target,0));
+
+    }
     static boolean search(int arr[],int target,int index)
     {
         if(index==arr.length)
@@ -8,10 +14,21 @@ public class LinearSearchInArray
         }
         return arr[index]==target || search(arr,target,index+1);
     }
-    public static void main(String[] args) {
-        int arr[]={1,5,78,100,25,63};
-        int target=85;
-        System.out.println(search(arr,target,0));
+
+    static int searchWithIndex(int arr[],int target,int index)
+    {
+        if(index==arr.length)
+        {
+            return -1;
+        }
+        if(arr[index]==target)
+        {
+            return index;
+        }
+        else
+        {
+            return searchWithIndex(arr,target,index+1);
+        }
 
     }
 }
