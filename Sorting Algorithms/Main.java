c9108973e4d0aa6f191ea9169cdd2258e0972790
Ayush Kushwaha <ayushkush00@gmail.com>
2026-04-17 10:10:22 +0530

    Sliding Window

---
+++ Sorting Algorithms/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Encapsulation s1= new Encapsulation();
+        s1.setName("Ayush");
+        s1.setAge(20);
+//        s1.getName();
+//        s1.getAge();
+        System.out.println("Name : "+s1.getName());
+        System.out.println("Age :"+s1.getAge());
+    }
+}
